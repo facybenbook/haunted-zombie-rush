17d14a9ba99c832859f3a600cf9fa3f237acb97d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-06-02 19:02:47 -0300

    deleted some folders
    deleted some folders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	[SerializeField] private GameObject mainMenu;
+	[SerializeField] public RawImage gameOverImage;
+
+	private bool playerActive = false;
+	private bool gameOver = false;
+	private bool gameStarted = false;
+
+	public bool PlayerActive {
+		get { return playerActive; }
+	}
+
+	public bool GameOver {
+		get { return gameOver; }
+	}
+
+	public bool GameStarted {
+		get { return gameStarted; }
+	}
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} 
+		else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		Assert.IsNotNull (mainMenu);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayerCollided() {
+		gameOverImage.enabled = true;
+		gameOver = true;
+	}
+
+	public void PlayerStartedGame() {
+		playerActive = true;
+	}
+
+	public void EnterGame(){
+		gameStarted = true;
+		mainMenu.SetActive (false);
+	}
+
+	public void RestartScene(){
+		Scene thisScene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(thisScene.name);
+		ResetAllProps ();
+	}
+
+	public void ResetAllProps(){
+		gameOverImage.enabled = false;
+		gameOver = false;
+		gameStarted = false;
+		playerActive = false;
+		mainMenu.SetActive (true);
+	}
+}
